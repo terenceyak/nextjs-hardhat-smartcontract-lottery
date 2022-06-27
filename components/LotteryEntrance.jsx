a8cb019ce4a935539b04eec874e276e303e8c5df
Terence Yak <terenceyak@addx.co>
2022-06-27 19:14:35 +0800

    Init commit

---
+++ components/LotteryEntrance.jsx
@@ -0,0 +1,102 @@
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useEffect,useState } from "react"
+import {ethers} from "ethers"
+import { useNotification } from "web3uikit"
+
+
+export default function LotteryEntrace() {
+    const {chainId: chainIdHex, isWeb3Enabled} = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ?  contractAddresses[chainId][0]: null
+    const [entranceFee, setEntranceFee] = useState("0")
+    const [numberPlayers, setNumberPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
+
+    const dispatch = useNotification()
+
+    const  {runContractFunction: enterRaffle, isLoading, isFetching } = useWeb3Contract({
+        abi:abi,
+        contractAddress:raffleAddress,
+        functionName: "enterRaffle",
+        params: {},
+        msgValue:entranceFee,
+    })
+
+    const  {runContractFunction: getEntranceFee} = useWeb3Contract({
+        abi:abi,
+        contractAddress:raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const  {runContractFunction: getNumPlayers} = useWeb3Contract({
+        abi:abi,
+        contractAddress:raffleAddress,
+        functionName: "getNumPlayers",
+        params: {},
+    })
+
+    const {runContractFunction: getRecentWinner} = useWeb3Contract({
+        abi:abi,
+        contractAddress:raffleAddress,
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    async function updateUI() {
+        if(raffleAddress) {
+            const entranceFeeFromCall = (await getEntranceFee()).toString()
+            const numPlayersFromCall = (await getNumPlayers()).toString()
+            const recentWinnerFromCall = await getRecentWinner()
+            setEntranceFee(entranceFeeFromCall)
+            setNumberPlayers(numPlayersFromCall)
+            setRecentWinner(recentWinnerFromCall)
+        }
+    }
+
+    useEffect(() => {
+        if(isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+        updateUI()
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+            type:"info",
+            message: "Transaction Complete!", 
+            title: "Tx Notification", 
+            position: "topR",
+            icon: "bell"
+        })
+    }
+    return (
+        <div>
+            Hi from lottery entrance!
+            { raffleAddress ? (<div>
+                <button 
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                    onClick={async function(){
+                    await enterRaffle({
+                        onSuccess: handleSuccess,
+                        onError: (error) => console.log(error),
+                    })
+                    }}
+                    disabled={isLoading || isFetching}
+                    >
+                        {isLoading || isFetching? (<div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>): (<div>Enter Raffle</div>)}</button>
+                <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee,"ether")} ETH</div>
+                <div>Number Players: {numberPlayers}</div>
+                <div>Recent Winner: {recentWinner}</div>
+            </div> ): (<div> 
+                No Raffle Address detected
+            </div>)}
+        </div>
+    ) 
+}  
---
+++ components/ManualHeader.jsx
@@ -0,0 +1,35 @@
+import {useMoralis} from 'react-moralis'
+import { useEffect } from 'react'
+
+export default function ManualHeader() {
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading} = useMoralis()
+    
+    useEffect(() => {
+        if(isWeb3Enabled) return 
+        if (typeof window != undefined)
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+    }, [])
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`Account changed to ${account}`)
+            if (account == null ) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("Null account found")
+            }
+        })
+    }, [])
+    return(<div>
+            {account ? (<div>Connected to {account}</div>):
+            <button onClick={async () => {
+                await enableWeb3()
+                window.localStorage.setItem("connected", "injected")
+            }} disabled = {isWeb3EnableLoading}
+        >
+                Connect
+            </button>}
+    </div>)
+}
